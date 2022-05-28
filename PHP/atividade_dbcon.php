33e8c5d380b1c194561b0abc88c7f7798de1e104
Anderson Viana <anderson@offertech.com.br>
2022-05-28 07:33:31 -0300

    files

---
+++ PHP/atividade_dbcon.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+    <head><title>ATIVIDADE IPM</title></head>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+            }
+
+        td, th {
+                border: 1px solid #dddddd;
+                text-align: left;ghjfhgfjhgjghkjghj
+                padding: 8px;
+                }
+
+        tr:nth-child(even) {
+                background-color: #dddddd;
+                }
+    </style>
+    <body>
+        <h1>Cidade</h1>
+        <table>
+        <tr>
+            <th>Código</th>
+            <th>Descrição</th>
+            <th>Ações</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Indaial</td>
+            <td><a href="atividade05.php">DELETE</a></td>
+        </tr>
+        </table>
+            <?php
+            $dbcon = pg_connect(
+                "host=localhost
+                port=5432
+                dbname=postgres
+                user=postgres
+                password=root
+                "
+            );
+            
+            function select_on_db($schema, $tabela, $coluna, $value) {
+                $query = "SELECT * FROM {$schema}.{$tabela}";
+                $dbresponse = pg_query($dbcon, $query);
+                pg_fetch_assoc($dbresponse);
+            }
+            function insert_on_db($schema, $tabela, $coluna, $value) {
+                $query = "INSERT INTO {$schema}.{$tabela} ($coluna) VALUES ($value)";
+                $dbresponse = pg_query($dbcon, $query);
+            }
+            function delete_on_db($schema, $tabela, $coluna, $value) {
+                $query = "DELETE FROM {$schema}.{$tabela} WHERE ($coluna) = VALUES ($value)";
+                $dbresponse = pg_query($dbcon, $query);
+            }
+            // echo $_POST['insert_city'];
+            ?>
+    </body>
+</html>
